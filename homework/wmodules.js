48bc08aa3e82548ddab8f355bece685c2fa4b18a
mkshoaf <sho15002@byui.edu>
2016-11-02 14:25:17 -0600

    lkj

---
+++ homework/wmodules.js
@@ -0,0 +1,6 @@
+//A jQuery fuction to implement modularization
+
+$(function() {
+    $("#wheader").load("wheader.html");
+    $("#wfooter").load("wfooter.html");
+});
---
+++ template/module.js
@@ -0,0 +1,7 @@
+//A jQuery fuction to implement modularization
+
+$(function() {
+    $("#page-header").load("header.html");
+    $("#page-nav").load("nav.html");
+    $("#page-footer").load("footer.html");
+});
