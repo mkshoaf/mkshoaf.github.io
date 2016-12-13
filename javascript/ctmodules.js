22eab77dfc26d09c63167594e55d2bf1fcc19795
mkshoaf <sho15002@byui.edu>
2016-12-12 17:11:13 -0700

    more work for final

--- javascript/ctmodules.js
+++ javascript/ctmodules.js
@@ -1,6 +1,6 @@
 //A jQuery fuction to implement modularization
 
 $(function () {
-    $("#ctpage-header").load("ctpage-header.html");
-    $("#ctpage-footer").load("ctpage-footer.html");
+    $("#ctpage-header").load("/final/ctpage-header.html");
+    $("#ctpage-footer").load("/final/ctpage-footer.html");
 });
